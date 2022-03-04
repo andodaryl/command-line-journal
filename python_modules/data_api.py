0746c7bbc36c265d184ca4f2ec421335194654f3
Daryl Ando <ando.daryl@hotmail.com>
2022-03-04 13:28:18 +0000

    Update filename + journal entry class + data validation

---
+++ python_modules/data_api.py
@@ -0,0 +1,72 @@
+'''
+Module for data manipulation and storage.
+'''
+
+# Imports
+from datetime import datetime
+
+# Data Types
+database = [] #remove once google sheets is fixed
+
+class JournalEntry:
+    '''
+    Creates journal entry instances.
+    '''
+    # Private variables
+    _timestamp = None
+    _text = ""
+
+    def __init__(self, text, timestamp = datetime.today()):
+        # id = stack
+        self.timestamp = timestamp
+        self.text = str(text)
+
+    @property
+    def date(self):
+        '''
+        Returns private date property i.e. read only.
+        '''
+        return datetime.date(self._timestamp)
+
+    @property
+    def time(self):
+        '''
+        Returns private time property i.e. read only.
+        '''
+        return datetime.time(self._timestamp)
+
+    def display(self):
+        '''
+        Prints details of journal entry.
+        '''
+        description = f'{self.date} {self.time} >>> {self.text}'
+        print(description)
+
+# CRUD Operations
+def create_entry(text):
+    '''
+    Adds new journal entry to database.
+    '''
+    new_entry = JournalEntry(text)
+    database.append(new_entry) # convert to google sheets API
+
+def get_entry(index):
+    '''
+    Retrieves existing journal entry.
+    '''
+
+    return database[index]
+
+def update_entry(index, text):
+    '''
+    Updates existing journal entry.
+    '''
+    if int(index): # convert to google sheets API
+        database[index].text = text
+
+def delete_entry(index):
+    '''
+    Deletes existing journal entry.
+    '''
+    if int(index): # convert to google sheets API
+        database.pop(index)
