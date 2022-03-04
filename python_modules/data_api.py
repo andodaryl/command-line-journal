baa6d1094e45c1e767c52117005ea0a20c1b0ac1
Daryl Ando <ando.daryl@hotmail.com>
2022-03-04 14:36:17 +0000

    Transfer external database behaviour

--- python_modules/data_api.py
+++ python_modules/data_api.py
@@ -4,6 +4,23 @@ Module for data manipulation and storage.
 
 # Imports
 from datetime import datetime
+from google.oauth2.service_account import Credentials
+import gspread
+
+# External Database
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+DATABASE = GSPREAD_CLIENT.open('clj_database')
+
+JOURNAL_ENTRIES = DATABASE.worksheet('journal_entries')
 
 # Data Types
 database = [] #remove once google sheets is fixed
--- run.py
+++ run.py
@@ -1,20 +1,3 @@
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-DATABASE = GSPREAD_CLIENT.open('clj_database')
-
-JOURNAL_ENTRIES = DATABASE.worksheet('journal_entries')
-
-data = JOURNAL_ENTRIES.get_all_values()
-
-print(data)
+'''
+Module that starts app.
+'''
