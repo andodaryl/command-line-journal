7ecfedaffbf40a7c8dcf8dbdc06d1384d44e5ce0
Daryl Ando <ando.daryl@hotmail.com>
2022-03-10 13:31:20 +0000

    Add basic structure for choice_api

--- python_modules/choice_api.py
+++ python_modules/choice_api.py
@@ -1,3 +1,24 @@
 '''
 Module for CLI user interaction
 '''
+
+# Imports
+from data_api import data_api as data
+from helper_api import helper_api as helper
+
+def _get_choice_api():
+    '''
+    Choice API namespace
+    '''
+
+    # Behaviours
+    placeholder = data
+
+    # Public API
+    _public_api = {
+        'placeholder': placeholder
+    }
+
+    return helper.namedtuple_from_dict('choice_api', _public_api)
+
+choice_api = _get_choice_api()
