12147b7e105953934977839157902500f1c5e968
Daryl Ando <ando.daryl@hotmail.com>
2022-03-11 01:17:47 +0000

    Add is_list + are_list_children

--- python_modules/helper_api.py
+++ python_modules/helper_api.py
@@ -24,10 +24,15 @@ def _get_helper_api():
 
     namedtuple_from_dict = lambda name, dict: namedtuple(name, dict.keys())(*dict.values())
 
+    is_list = lambda obj: isinstance(obj, list)
+    are_list_children = lambda obj, instance: all(isinstance(child, instance) for child in obj)
+
     # Public API
     _public_api = {
       'namedtuple_from_dict': namedtuple_from_dict,
-      'get_index': get_index
+      'get_index': get_index,
+      'is_list': is_list,
+      'are_list_children': are_list_children
     }
 
     return namedtuple_from_dict('helper_api', _public_api)
