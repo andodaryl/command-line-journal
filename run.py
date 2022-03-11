4ba4b3bc3899c4ffdd461b5657d48abb26ab0301
Daryl Ando <ando.daryl@hotmail.com>
2022-03-11 12:41:57 +0000

    Add  structure + placeholders for run.py

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 '''
 Module that starts app.
 '''
+from python_modules.display_api import display_api as display
+
+def main():
+    '''
+    Container for main behaviour for application start.
+    '''
+    # Behaviour
+    return display
+
+main()
