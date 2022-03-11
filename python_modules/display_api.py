23a9ba7961a51b677b0fff46294db6e2c0ad0532
Daryl Ando <ando.daryl@hotmail.com>
2022-03-11 12:36:39 +0000

    Add display behaviour & replace {} with dict()

--- python_modules/display_api.py
+++ python_modules/display_api.py
@@ -10,13 +10,41 @@ def _get_display_api():
     '''
     Namespace for Display API.
     '''
+    # Config
+    _display = dict(
+      welcome_msg = 'Welcome to Command Line Journal!\n',
+      goodbye_msg = 'See you next time!\n',
+    )
+
+    display = helper.namedtuple_from_dict('display', _display)
+
     # Behaviour
-    placeholder = choice
+    greet_user = lambda: print(display.welcome_msg)
+    farewell_user = lambda: print(display.welcome_msg)
+
+    def go_to_main_menu():
+        '''
+        Displays main menu.
+        '''
+        choices = [
+          ('Exit', exit),
+          ('Show All Journals', choice.get_all_entries),
+          ('Create Journal', choice.create_entry),
+          ('Retrieve Journal', choice.get_entry),
+          ('Update Journal', choice.update_entry),
+          ('Delete Journal', choice.delete_entry),
+        ]
+
+        wait_for_keypress = choice.bind_keys(choices)
+
+        wait_for_keypress()
 
     # Public API
-    _public_api = {
-        'placeholder': placeholder
-    }
+    _public_api = dict(
+        greet_user = greet_user,
+        farewell_user = farewell_user,
+        go_to_main_menu = go_to_main_menu
+    )
 
     return helper.namedtuple_from_dict('display_api', _public_api)
 
